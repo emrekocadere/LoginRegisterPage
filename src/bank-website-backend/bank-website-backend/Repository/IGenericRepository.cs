97f3c7ce718c70bf94203196a052a78a60fce54b
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-10 20:43:40 +0300

    Add : IGenericRepository<T> interface for generic repository operations

---
+++ src/bank-website-backend/bank-website-backend/Repository/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using bank_website_backend.Entities;
+
+namespace bank_website_backend.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public List<T> GetAlls();
+        public bool AddRecord(T entity);
+        public T FindById(int id);
+        public bool DeleteRecord(T entity);
+    }
+}
