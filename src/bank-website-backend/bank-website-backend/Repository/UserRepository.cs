6d9f7f60335c7dd0a58ba36e7ea640a4097ff76b
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-10 23:50:01 +0300

    Add : UserRepository class for user repository operations

---
+++ src/bank-website-backend/bank-website-backend/Repository/UserRepository.cs
@@ -0,0 +1,74 @@
+﻿using bank_website_backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace bank_website_backend.Repository
+{
+    public class UserRepository:IUserRepository
+    {
+        BankWebsiteDbContext dbContext;
+        UserRepository(BankWebsiteDbContext dbContext) {
+             this.dbContext = dbContext;
+        }
+
+        public bool AddRecord(User entity)
+        {
+            try
+            {
+                dbContext.Users.Add(entity);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+
+
+        public bool DeleteRecord(User entity)
+        {
+            try
+            {
+                dbContext.Users.Remove(entity);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+        }
+
+        public User FindById(int id)
+        {
+            try
+            {
+                return dbContext.Users.Find(id);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+            
+        }
+
+        public List<User> GetAlls()
+        {
+            try
+            {
+                return dbContext.Users.ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+
+    }
+}
