51ddb1faf5ead163a31a75501ad3fa2b48f45b27
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-10 17:12:53 +0300

    Generate EF entities from bank-website-DB(DB First)

---
+++ src/bank-website-backend/bank-website-backend/Entities/BankWebsiteDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace bank_website_backend.Entities;
+
+public partial class BankWebsiteDbContext : DbContext
+{
+    public BankWebsiteDbContext()
+    {
+    }
+
+    public BankWebsiteDbContext(DbContextOptions<BankWebsiteDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=EMREVIVO;Database=bank-website-DB;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("User");
+
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Password).HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ src/bank-website-backend/bank-website-backend/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bank_website_backend.Entities;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Password { get; set; }
+}
