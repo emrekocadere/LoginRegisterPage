c4eae1ad72a5c4130b230e07dda136652853bd08
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-10 17:23:26 +0300

    Add IUserRepository interface for user management

---
+++ src/bank-website-backend/bank-website-backend/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using bank_website_backend.Entities;
+
+namespace bank_website_backend.Repository
+{
+    public interface IUserRepository
+    {
+        public List<User> GetAlls();
+        public User AddUser();
+        public List<User> FindById();
+        public bool DeleteUser();
+    }
+}
