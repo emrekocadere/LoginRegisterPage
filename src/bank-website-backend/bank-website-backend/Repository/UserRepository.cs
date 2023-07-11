456e32be4f29088882d05514a1b7221e3adac032
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-11 13:25:29 +0300

    Refactor : UserRepository class with name validation functionality

--- src/bank-website-backend/bank-website-backend/Repository/UserRepository.cs
+++ src/bank-website-backend/bank-website-backend/Repository/UserRepository.cs
@@ -3,74 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace bank_website_backend.Repository
 {
-    public class UserRepository:IUserRepository
+    public class UserRepository:GenericRepository<User>,IUserRepository
     {
         BankWebsiteDbContext dbContext;
-        public UserRepository(BankWebsiteDbContext dbContext) {
+        public UserRepository(BankWebsiteDbContext dbContext):base(dbContext) {
              this.dbContext = dbContext;
         }
 
-        public bool AddRecord(User entity)
+        public bool IsNameExsist(string name)
         {
-            try
+            if (dbContext.Users.FirstOrDefault(u => u.Name == name) != null)
             {
-                dbContext.Users.Add(entity);
-                dbContext.SaveChanges();
                 return true;
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.ToString());
+            else
                 return false;
-            }
         }
-
-
-
-        public bool DeleteRecord(User entity)
-        {
-            try
-            {
-                dbContext.Users.Remove(entity);
-                dbContext.SaveChanges();
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return false;
-            }
-
-        }
-
-        public User FindById(int id)
-        {
-            try
-            {
-                return dbContext.Users.Find(id);
-
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return null;
-            }
-            
-        }
-
-        public List<User> GetAlls()
-        {
-            try
-            {
-                return dbContext.Users.ToList();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return null;
-            }
-        }
-
 
     }
 }
