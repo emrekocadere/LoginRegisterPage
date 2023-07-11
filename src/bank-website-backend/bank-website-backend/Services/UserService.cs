80ff5b185d5bc6353a3a412f147acd3c9c1b4222
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-11 13:27:39 +0300

    Add : UserService class for user service operations

---
+++ src/bank-website-backend/bank-website-backend/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using bank_website_backend.Entities;
+using bank_website_backend.Repository;
+using System.Globalization;
+
+namespace bank_website_backend.Services
+{
+    public class UserService : IUserService
+    {
+        IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public bool IsNameExsist(string name)
+        {
+            return _userRepository.IsNameExsist(name);
+        }
+    }
+}
