c033ee2808a34973119203a36e9df49ae7308d6c
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-11 14:09:51 +0300

    Add : MapperConfig class for AutoMapper configuration

---
+++ src/bank-website-backend/bank-website-backend/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using bank_website_backend.Entities;
+using bank_website_backend.Model.Request;
+
+namespace bank_website_backend
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<User,RegisterRequest>().ReverseMap();
+        }
+    }
+}
