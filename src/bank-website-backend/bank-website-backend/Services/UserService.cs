1a0890e0f59845d8b9a25fb21d456001b71a4a35
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-11 14:08:55 +0300

    Refactor : UserService class with additional registration functionality

--- src/bank-website-backend/bank-website-backend/Services/UserService.cs
+++ src/bank-website-backend/bank-website-backend/Services/UserService.cs
@@ -1,19 +1,33 @@
-﻿using bank_website_backend.Entities;
+﻿using AutoMapper;
+using bank_website_backend.Entities;
+using bank_website_backend.Model.Request;
 using bank_website_backend.Repository;
+using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 
 namespace bank_website_backend.Services
 {
-    public class UserService : IUserService
+    public class UserService :IUserService
     {
         IUserRepository _userRepository;
-        public UserService(IUserRepository userRepository)
+        private readonly IMapper _mapper;
+        public UserService(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
         public bool IsNameExsist(string name)
         {
             return _userRepository.IsNameExsist(name);
         }
+
+        public User MapUserAndUserRequest(RegisterRequest registerRequest)
+        {
+            return _mapper.Map<User>(registerRequest);
+        }
+        public bool Register(User user)
+        {
+            return _userRepository.AddRecord(user);
+        }
     }
-}
+} 
