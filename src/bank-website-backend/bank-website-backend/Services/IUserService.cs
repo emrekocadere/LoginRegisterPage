b8e5e9102e1f91710acee81798eae78a6c08037f
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-11 14:08:03 +0300

    Update : IUserService interface with additional methods for user registration

--- src/bank-website-backend/bank-website-backend/Services/IUserService.cs
+++ src/bank-website-backend/bank-website-backend/Services/IUserService.cs
@@ -1,7 +1,12 @@
-﻿namespace bank_website_backend.Services
+﻿using bank_website_backend.Entities;
+using bank_website_backend.Model.Request;
+
+namespace bank_website_backend.Services
 {
     public interface IUserService
     {
         public bool IsNameExsist(string name);
+        public User MapUserAndUserRequest(RegisterRequest registerRequest);
+        public bool Register(User user);
     }
 }
