fbc992d4fe961af43328e94cef29add4225b696a
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-16 22:27:29 +0300

    Add : UserController with Register endpoint

---
+++ src/bank-website-backend/bank-website-backend/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using bank_website_backend.Entities;
+using bank_website_backend.Model.Request;
+using bank_website_backend.Repository;
+using bank_website_backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bank_website_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService) {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            var user=_userService.MapUserAndUserRequest(request);
+            return Ok(_userService.Register(request));
+        }
+    }
+}
+
+
