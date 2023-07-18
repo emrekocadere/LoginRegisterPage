c4829fcedc31b122cd18ef2daffc6fe08cd45945
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-18 10:51:15 +0300

    feat : Update UserService with Login method

--- src/bank-website-backend/bank-website-backend/Services/UserService.cs
+++ src/bank-website-backend/bank-website-backend/Services/UserService.cs
@@ -13,11 +13,13 @@ namespace bank_website_backend.Services
         IUserRepository _userRepository;
         private readonly IMapper _mapper;
         private readonly UserManager<TUser> _userManager; 
-        public UserService(IUserRepository userRepository, IMapper mapper, UserManager<TUser> userManager)
+        private readonly SignInManager<TUser> _signInManager;
+        public UserService(IUserRepository userRepository, IMapper mapper, UserManager<TUser> userManager,SignInManager<TUser> signInManager)
         {
             _userRepository = userRepository;
             _mapper = mapper;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public async Task<IdentityResult> Register(RegisterRequest registerRequest)
@@ -27,6 +29,11 @@ namespace bank_website_backend.Services
                 Email=registerRequest.Email,
                 PhoneNumber=registerRequest.Phone,       
             },registerRequest.Password);
+        }
+
+        public async Task<Microsoft.AspNetCore.Identity.SignInResult> Login(LoginRequest loginRequest)
+        {
+            return await _signInManager.PasswordSignInAsync(loginRequest.Name, loginRequest.Password,false,true);
         }
     }
 } 
