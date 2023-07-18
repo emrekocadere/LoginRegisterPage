35564f0aa83db4b4919b0742c66defdeb8cfa3c1
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-18 10:29:46 +0300

    Update : UserService with asynchronous registration and UserManager

--- src/bank-website-backend/bank-website-backend/Services/UserService.cs
+++ src/bank-website-backend/bank-website-backend/Services/UserService.cs
@@ -2,6 +2,7 @@
 using bank_website_backend.Entities;
 using bank_website_backend.Model.Request;
 using bank_website_backend.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
 
@@ -11,30 +12,21 @@ namespace bank_website_backend.Services
     {
         IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public UserService(IUserRepository userRepository, IMapper mapper)
+        private readonly UserManager<TUser> _userManager; 
+        public UserService(IUserRepository userRepository, IMapper mapper, UserManager<TUser> userManager)
         {
             _userRepository = userRepository;
             _mapper = mapper;
-        }
-        public bool IsNameExsist(string name)
-        {
-            return _userRepository.IsNameExsist(name);
+            _userManager = userManager;
         }
 
-        public User MapUserAndUserRequest(RegisterRequest registerRequest)
+        public async Task<IdentityResult> Register(RegisterRequest registerRequest)
         {
-            return _mapper.Map<User>(registerRequest);
-        }
-        public bool Register(RegisterRequest user)
-        {
-            if(!IsNameExsist(user.Name))
-            {
-                return _userRepository.AddRecord(new User {Name=user.Name,Password=user.Password});          
-            }
-            else
-            {
-                return false;
-            }
+            return await _userManager.CreateAsync(new TUser() {
+                UserName=registerRequest.Name,
+                Email=registerRequest.Email,
+                PhoneNumber=registerRequest.Phone,       
+            },registerRequest.Password);
         }
     }
 } 
