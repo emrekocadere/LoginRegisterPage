13fe5ddd277a151153f122bc3d2016a13db1b42c
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-18 10:28:59 +0300

    Refactor : IUserService interface by removing unused methods

--- src/bank-website-backend/bank-website-backend/Services/IUserService.cs
+++ src/bank-website-backend/bank-website-backend/Services/IUserService.cs
@@ -6,8 +6,7 @@ namespace bank_website_backend.Services
 {
     public interface IUserService
     {
-        public bool IsNameExsist(string name);
-        public User MapUserAndUserRequest(RegisterRequest registerRequest);
+
         public  Task<IdentityResult> Register(RegisterRequest registerRequest);
     }
 }
