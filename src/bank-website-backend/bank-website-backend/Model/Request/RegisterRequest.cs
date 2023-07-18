12ab7b6a08505c14f4a9a642f08b754c0f441d62
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-18 09:44:23 +0300

    Update : RegisterRequest Model

--- src/bank-website-backend/bank-website-backend/Model/Request/RegisterRequest.cs
+++ src/bank-website-backend/bank-website-backend/Model/Request/RegisterRequest.cs
@@ -4,5 +4,7 @@
     {
         public string Name { get; set; }
         public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
     }
 }
