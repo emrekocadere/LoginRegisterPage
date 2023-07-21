cef22a4181ec640bb65d02e14f4250abed3ced11
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-07-21 11:09:24 +0300

    Update : BankWebsiteDbContext to inherit from IdentityDbContext with custom types

--- src/bank-website-backend/bank-website-backend/Entities/BankWebsiteDbContext.cs
+++ src/bank-website-backend/bank-website-backend/Entities/BankWebsiteDbContext.cs
@@ -1,38 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace bank_website_backend.Entities;
 
-public partial class BankWebsiteDbContext : DbContext
+public partial class BankWebsiteDbContext : IdentityDbContext<TUser, Role, string>
 {
-    public BankWebsiteDbContext()
-    {
-    }
 
-    public BankWebsiteDbContext(DbContextOptions<BankWebsiteDbContext> options)
-        : base(options)
-    {
-    }
+    public BankWebsiteDbContext(DbContextOptions<BankWebsiteDbContext> options): base(options) { }
 
-    public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=EMREVIVO;Database=bank-website-DB;Trusted_Connection=True;TrustServerCertificate=True");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.ToTable("User");
-
-            entity.Property(e => e.Name).HasColumnName("name");
-            entity.Property(e => e.Password).HasColumnName("password");
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
